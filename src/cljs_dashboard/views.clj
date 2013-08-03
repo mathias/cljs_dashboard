4d7c2ecdcff5b205adafddd6dce004b97a270aae
Matt Gauger <dev+mathias@bendyworks.com>
2013-08-02 23:24:34 -0500

    Fonts

--- src/cljs_dashboard/views.clj
+++ src/cljs_dashboard/views.clj
@@ -10,9 +10,17 @@
     [:head
       [:title title]
      (include-css "css/bootstrap.min.css")
+     (include-css "//fonts.googleapis.com/css?family=Roboto")
      (include-css "css/statusboard.css")]
     [:body
      [:div {:class "container-fluid"}
       [:h1 title]
-      [:div {:id "widgets-holder" :class "row-fluid"}]
-      (include-js "/js/main.js")]]))
+      [:div {:id "widgets-holder" :class "row-fluid"}
+       [:div {:class "span-2 show bigrow"}
+        [:div {:class "row-fluid grid-margins rowlabels"}
+         [:div {:class "span12 heavy dateheader"} "Bus Arrivals"]]
+        [:div {:class "row-fluid grid-margins"}
+         [:div {:class "span12 heavy eventname"}
+          [:span {:class "predate"} "8:46PM"]
+          6]]]]]
+      (include-js "/js/main.js")]))
